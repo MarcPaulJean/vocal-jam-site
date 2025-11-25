8fe7db84a96fa207214b0b816477c36d8f516f2c
MarcPaulJean <alinatmarc@yahoo.fr>
2025-11-25 11:34:29 +0100

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Assure que les chemins sont relatifs pour l'hébergement
+  base: './',
   build: {
     outDir: 'dist',
     sourcemap: false
